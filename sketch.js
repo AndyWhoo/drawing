0098e01fc10932700ef649db1e337e5ced8333d9
Andy Wu <33040315+AndyWhoo@users.noreply.github.com>
2019-10-07 22:17:14 -0700

    perlin noise

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
+let noiseOffset = 0.0;
+let strokeWidth = 5
+
 function setup() {
   createCanvas(600, 600);
 
   background(203, 100, 130);
-  strokeWeight(4);
 }
 
 function draw() {
+  background(203, 100, 130);
+  strokeWeight(strokeWidth);
+
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
 
   if (mouseIsPressed) {
     stroke(map(mouseX, 100, 600, 45, 200, true), map(mouseX, 34, 600, 220, 200, true))
-    line(width - mouseX, width - mouseY, width - pmouseX, pmouseY);
+    line(width - mouseX, mouseY,width - pmouseX,  pmouseY);
+    line(width - mouseX, mouseY, pmouseX,  pmouseY);
+
 
   }
 
@@ -20,8 +29,8 @@ function keyTyped() {
   //console.log(`key ${key} is being typed`)
 
   if (key === 's') {
-saveCanvas('fileName', 'png')
+    saveCanvas('fileName', 'png')
   }
-return false;
+  return false;
 
 }
