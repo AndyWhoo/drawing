beaa3f573cdf562ede6e70298f2e354698f6ea46
Andy Wu <33040315+AndyWhoo@users.noreply.github.com>
2019-10-07 21:40:17 -0700

    made drawing site

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+
+  background(203, 100, 130);
+  strokeWeight(4);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 100, 600, 45, 200, true), map(mouseX, 34, 600, 220, 200, true))
+    line(width - mouseX, width - mouseY, width - pmouseX, pmouseY);
+
+  }
+
+}
+
+function keyTyped() {
+
+  //console.log(`key ${key} is being typed`)
+
+  if (key === 's') {
+saveCanvas('fileName', 'png')
+  }
+return false;
+
 }
