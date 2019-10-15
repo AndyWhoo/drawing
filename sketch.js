c7a97d6394b58e6b43d87de482dc2862d700736d
Andy Wu <33040315+AndyWhoo@users.noreply.github.com>
2019-10-15 15:23:18 -0700

    yes

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@
 
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(400, 400);
 
   background(0, 0, 0);
   //fill(0, 45, 250);
@@ -16,16 +16,16 @@ function draw() {
   //noiseOffset += 0.5;
   //strokeWidth = noise(noiseOffset) * 30;
   if (mouseIsPressed) {
-//strokeWeight(10);
-strokeWeight(20);
+    //strokeWeight(10);
+    strokeWeight(20);
     line(mouseX, mouseY, pmouseX, pmouseY);
 
-    stroke(map(mouseX, 0, 0, 255, 0, true), map(mouseX, 100,600 ,400 , 400, true) )
-    strokeWeight(160);
+    stroke(map(mouseX, 0, 0, 255, 0, true), map(mouseX, 100, 600, 400, 400, true))
+    strokeWeight(170);
 
-line(mouseX*5,mouseY,pmouseX *5, pmouseY );
+    line(mouseX * 5, mouseY,  pmouseX * 5, pmouseY);
 
-line(height - mouseX, width - mouseY, height - pmouseX, width - pmouseY);
+    line(height - mouseX, width - mouseY, height - pmouseX, width - pmouseY);
 
     stroke(255);
 
